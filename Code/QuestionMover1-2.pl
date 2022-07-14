238f2ce92973f60849e0f78efa0349d7e1b953e6
IllyaKierkosz <Kierkoszi@gmail.com>
2022-07-14 18:35:35 -0300

    Modifying the question mover subroutine to access base file structure properly.

--- Code/QuestionMover1-2.pl
+++ Code/QuestionMover1-2.pl
@@ -8,45 +8,48 @@ Qmove();
 
 
 sub Qmove{
-my $Dir = "InputFiles";
-#Need an "InputFiles" directory. This is where the moodle lessons to be searched go.
+mkdir("Questions") or die "Could not make Question directory: $!";
+#Make the directory where the harvested questions go.
+opendir(ACTIVITIES, "InputFiles/activities") or die "Could not open Input Files directory: $!";
+my @inputContents = readdir(ACTIVITIES);
+my @lessonDirectories = grep(m{lesson}, @inputContents);
+print "Lessons in this backup:\n";
+print "@lessonDirectories\n";
+#Generating a list of lesson sub-directories.
 
-mkdir("Questions") or die "couldn't make Question directory: $!";
-#Make the directory where the harvested questions go
-
-opendir(DIR, $Dir) or die "Couldn't open input file directory: $!";
-while (my $file = readdir(DIR)) {
-	my $filetarget = "lesson";
-	if ($file =~ /\Q$filetarget\E/){
-	print "Working on $file\n";
-	open(my $read, "<","$Dir/$file") or die "Couldn't open input file $file: $!\n";
-	open(my $outext, ">", "Questions/Questions-$file") or die "Couldn't create output file for $file: $!";
+foreach(@lessonDirectories){
+	my $currentLesson = $_;
+	print "Working on $currentLesson\n";
+	open(my $intext, "<","/$currentLesson/lesson.xml") or die "Couldn't open lesson file for $currentLesson: $!\n";
+	open(my $outext, ">", "Questions/Questions-$currentLesson") or die "Couldn't create output file for $currentLesson: $!";
 		my $target = "<qtype>3</qtype>"; my $targetClose = "</page>";
+		#target "phrases" selecting for question blocks.
 		my @workingText;
-		while(my $comptext = <$read>){
+		while(my $comptext = <$intext>){
 			chomp $comptext;
 			push(@workingText, $comptext);
 		}
+		#moving contents of the lesson file into a working text array.
 		my $index = @workingText-1;
 		my $i = 0;
 		my $questionIndex = 0;
 		while($i<=$index){
-			if($workingText[$i] =~ /\Q$target\E/){
+			if($workingText[$i] =~ m{$target}){
 				$questionIndex++;
 				print $outext "Question $questionIndex:\n";
 				do{
 					print $outext "$workingText[$i]\n";
 					$i++;
-				} while($workingText[$i] !~ /\Q$targetClose\E/);
+				} while($workingText[$i] !~ m{$targetClose});
 				print $outext "$workingText[$i]\n";
 				print $outext "\n";
 			}
 			$i++;
 		
 		}
+		#Searches each line of the working text array for the target phrases, and prints the contents between to the output file.
 	close($outext);
-	close($read);
-	}
+	close($intext);
 }
-closedir(DIR);
+closedir(ACTIVITIES)
 }
